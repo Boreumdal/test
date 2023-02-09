961b0ef7251c9ea7ea9bc771648b233d2f8ce84c
Melvin Arellano <lmelvin.arellano@gmail.com>
2023-02-09 21:58:45 +0800

    event day

--- frontend/src/components/Dashboard/AddEvent.jsx
+++ frontend/src/components/Dashboard/AddEvent.jsx
@@ -3,27 +3,33 @@ import { useSystem } from '../../context/SystemContext'
 import axios from 'axios'
 
 const AddEvent = () => {
-    const { data } = useSystem()
+    const { data, notif, setNotif } = useSystem()
     const [img, setImg] = useState('')
-    const title = useRef(null)
-    const description = useRef(null)
-    const campus = useRef(null)
-    const when = useRef(null)
+    const [title, setTitle] = useState('')
+    const [description, setDescription] = useState('')
+    const [campus, setCampus] = useState('')
+    const [when, setWhen] = useState('')
 
     const inputStyle = 'block border shadow rounded w-full text-sm py-2 px-3 border-gray-300'
     const defaultImage = 'https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_960_720.png'
 
+    const reset = () => {
+        setImg('')
+        setTitle('')
+        setDescription('')
+        setWhen('')
+    }
+
     const handleAddEvent = e => {
         e.preventDefault()
-
-        axios.post('http://localhost:8000/dashboard/student/add', {
-            title: title.current.value,
-            description: description.current.value,
-            campus: campus.current.value,
-            when: when.current.value,
-            picture: img
-        })
+        console.log('sub')
+        axios.post('http://localhost:8000/dashboard/event/add', { title, description, campus, when, picture: img })
+            .then(response => {
+                setNotif(response.data)
+                reset()
+            })
     }
+    
 
   return (
     <div className='mx-6 mt-2'>
@@ -35,22 +41,22 @@ const AddEvent = () => {
                     <h3 className='text-xs font-bold text-gray-500 py-2'>Event Information</h3>
                     <div className='input-field-addstudent'>
                         <label htmlFor="title">Title:</label>
-                        <input type="text" className={inputStyle} id='title' placeholder='Enter event title...' />
+                        <input type="text" onChange={e => setTitle(e.target.value)} className={inputStyle} id='title' value={title} placeholder='Enter event title...' />
                     </div>
                     <div className='input-field-addstudent'>
                         <label htmlFor="description" className='py-2 self-start'>Description:</label>
-                        <textarea className={inputStyle} id='description' placeholder='Lorem ipsum dolor sit amet, consectetur adipisicing...' rows='4' />
+                        <textarea className={inputStyle} onChange={e => setDescription(e.target.value)} id='description' value={description} placeholder='Lorem ipsum dolor sit amet, consectetur adipisicing...' rows='4' />
                     </div>
                     <div className='input-field-addstudent'>
                         <label htmlFor="campus">Campus:</label>
-                        <select className={inputStyle} id='campus'>
+                        <select className={inputStyle} onChange={e => setCampus(e.target.value)} id='campus'>
                             <option value="Main">Cainta Main Campus</option>
                             <option value="Taytay">Taytay Campus</option>
                         </select>
                     </div>
                     <div className='input-field-addstudent'>
                         <label htmlFor="when">When:</label>
-                        <input type="date" className={inputStyle} id='when' />
+                        <input type="date" onChange={e => setWhen(e.target.value)} className={inputStyle} id='when' value={when} />
                     </div>
                 </div>
                 <div className='w-1/2'>
@@ -59,14 +65,20 @@ const AddEvent = () => {
                         <label htmlFor="profile_pic" className='py-2 self-start'>Event Snapshot:</label>
                         <div className='flex flex-col gap-2'>
                             <img src={img ? img : defaultImage} className='w-full shadow h-[200px] object-cover object-center' alt="" />
-                            <input type="text" onChange={e => setImg(e.target.value)} className={inputStyle + ' self-start'} id='profile_pic' placeholder='Snapshot link...' />
+                            <input type="text" onChange={e => setImg(e.target.value)} className={inputStyle + ' self-start'} id='profile_pic' value={img} placeholder='Snapshot link...' />
                         </div>
                     </div>
                 </div>
             </div>
-            <div className='flex flex-row items-center mt-2 gap-2'>
-                <button type="submit" className='border-2 border-orange-500 text-white bg-orange-500 hover:text-orange-500 hover:bg-transparent py-1 px-3 rounded font-semibold duration-200 text-sm shadow-sm'>Add Event</button>
-                <span className='font-medium text-xs text-gray-400'>Your admin id is: { data._id }</span>
+            <div className='flex flex-row justify-between items-center mt-2 gap-2'>
+                <div className='flex items-center gap-2'>
+                    <button type="submit" className='border-2 border-orange-500 text-white bg-orange-500 hover:text-orange-500 hover:bg-transparent py-1 px-3 rounded font-semibold duration-200 text-sm shadow-sm'>Add Event</button>
+                    { notif?.msg && <p className='text-xs text-green-500 rounded-full py-1 font-medium'>{ notif.msg }</p> }
+                    { notif?.err && <p className='text-xs text-red-500 rounded-full py-1 font-medium'>{ notif.err }</p> }
+                </div>
+                <p className='font-medium text-xs text-gray-400'>Your admin id is: { data._id }</p>
+                
+                
             </div>
 
         </form>
--- frontend/src/context/SystemContext.jsx
+++ frontend/src/context/SystemContext.jsx
@@ -1,13 +1,21 @@
-import { createContext, useContext, useState } from "react";
+import { createContext, useContext, useEffect, useState } from "react";
 
 const SystemContext = createContext(null)
 
 export const SysContext = ({ children }) => {
     const [token, setToken] = useState('')
     const [data, setData] = useState({})
+    const [notif, setNotif] = useState({})
+
+    useEffect(() => {
+        const interval = setInterval(() => {
+            setNotif({})
+        }, 2400)
+        return () => clearInterval(interval)
+    }, [])
     
     return (
-        <SystemContext.Provider value={{token, setToken, data, setData}}>
+        <SystemContext.Provider value={{token, setToken, data, setData, notif, setNotif}}>
             { children }
         </SystemContext.Provider>
     )
