d6206da56fa929744cee77000efa9c2900940d76
Melvin Arellano <lmelvin.arellano@gmail.com>
2023-02-10 19:56:31 +0800

    use id as username done

--- frontend/src/components/Dashboard/AddStudent.jsx
+++ frontend/src/components/Dashboard/AddStudent.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import { useSystem } from '../../context/SystemContext'
 
 const AddStudent = () => {
@@ -15,9 +15,9 @@ const AddStudent = () => {
     const [status, setStatus] = useState('Regular')
     const [contact, setContact] = useState('')
     const [email, setEmail] = useState('')
-    const [profile, setProfile] = useState('')
     const [uname, setUname] = useState('')
     const [password, setPassword] = useState('')
+    const [idAsUsername, setIdAsUsername] = useState(false)
 
     const reset = () => {
         setFname('')
@@ -37,8 +37,27 @@ const AddStudent = () => {
         setImg('')
     }
 
+    const handleIdAsUsername = () => {
+
+    }
+
+    // console.log(uname + '  ' + studentId)
+
+    useEffect(() => {
+        if (idAsUsername){
+            setUname(studentId)
+        }
+    }, [idAsUsername])
+
     const handleAddStudent = e => {
-        
+        e.preventDefault()
+
+
+    }
+    const idAsUsernameFunc = e => {
+        const val = e.target.value
+        setStudentId(val)
+        setUname(val)
     }
 
     const inputStyle = 'block border shadow rounded w-full text-sm py-2 px-3 border-gray-300'
@@ -54,14 +73,14 @@ const AddStudent = () => {
                     <div className='input-field-addstudent'>
                         <label htmlFor="name">Name:</label>
                         <div className='flex gap-1'>
-                            <input type="text" className={inputStyle} placeholder='First' />
-                            <input type="text" className={inputStyle} placeholder='Middle' />
-                            <input type="text" className={inputStyle} placeholder='Last' />
+                            <input type="text" onChange={e => setFname(e.target.value)} value={fname} className={inputStyle} placeholder='First' />
+                            <input type="text" onChange={e => setMname(e.target.value)} value={mname} className={inputStyle} placeholder='Middle' />
+                            <input type="text" onChange={e => setLname(e.target.value)} value={lname} className={inputStyle} placeholder='Last' />
                         </div>
                     </div>
                     <div className='input-field-addstudent'>
                         <label htmlFor="">Gender:</label>
-                        <select className={inputStyle}>
+                        <select className={inputStyle} value={gender} onChange={e => setGender(e.target.value)}>
                             <option value="Male">Male</option>
                             <option value="Female">Female</option>
                         </select>
@@ -72,11 +91,11 @@ const AddStudent = () => {
                     <h3 className='text-xs font-bold text-gray-500 py-2'>Contact Information</h3>
                     <div className='input-field-addstudent'>
                         <label htmlFor="contact">Contact Number:</label>
-                        <input type="number" className={inputStyle} id='contact' placeholder='Contact Number...' />
+                        <input type="number" onChange={e => setContact(e.target.value)} value={contact} className={inputStyle} id='contact' placeholder='Contact Number...' />
                     </div>
                     <div className='input-field-addstudent'>
                         <label htmlFor="email">Email:</label>
-                        <input type="number" className={inputStyle} id='email' placeholder='Email...' />
+                        <input type="number" onChange={e => setEmail(e.target.value)} value={email} className={inputStyle} id='email' placeholder='Email...' />
                     </div>
                 </div>
             </div>
@@ -86,29 +105,29 @@ const AddStudent = () => {
                     <h3 className='text-xs font-bold text-gray-500 py-2'>Student Information</h3>
                     <div className='input-field-addstudent'>
                         <label htmlFor="student_id">Student ID:</label>
-                        <input type="text" className={inputStyle} id='student_id' placeholder='TA23XXXX' />
+                        <input type="text" onChange={e => idAsUsername ? idAsUsernameFunc(e) : setStudentId(e.target.value)} value={studentId} className={inputStyle} id='student_id' placeholder='TA23XXXX' />
                     </div>
                     <div className='input-field-addstudent'>
                         <label htmlFor="year">Year Level:</label>
-                        <input type="number" className={inputStyle} id='year' placeholder='4' />
+                        <input type="number" onChange={e => setYear(e.target.value)} value={year} className={inputStyle} id='year' placeholder='4' />
                     </div>
                     <div className='input-field-addstudent'>
                         <label htmlFor="course">Course:</label>
-                        <select className={inputStyle} id='course'>
+                        <select className={inputStyle} value={course} onChange={e => setCourse(e.target.value)} id='course'>
                             <option value="BSIT">BSIT</option>
                             <option value="BSCS">BSCS</option>
                         </select>
                     </div>
                     <div className='input-field-addstudent'>
                         <label htmlFor="branch">Branch:</label>
-                        <select className={inputStyle} id='branch'>
+                        <select className={inputStyle} value={branch} onChange={e => setBranch(e.target.value)} id='branch'>
                             <option value="Main">Main</option>
                             <option value="Taytay">Taytay</option>
                         </select>
                     </div>
                     <div className='input-field-addstudent'>
                         <label htmlFor="status">Status:</label>
-                        <select className={inputStyle} id='status'>
+                        <select className={inputStyle} value={status} onChange={e => setStatus(e.target.value)} id='status'>
                             <option value="Regular">Regular</option>
                             <option value="Irregular">Irregular</option>
                         </select>
@@ -120,28 +139,28 @@ const AddStudent = () => {
                     <div className='input-field-addstudent'>
                         <label htmlFor="profile_pic" className='py-2 self-start'>Student Picture:</label>
                         <div className='input-field-addstudent-preview'>
-                            <input type="text" className={inputStyle + ' self-start'} id='profile_pic' placeholder='Student picture link...' />
+                            <input type="text" onChange={e => setImg(e.target.value)} value={img} className={inputStyle + ' self-start'} id='profile_pic' placeholder='Student picture link...' />
                             <img src={img ? img : defaultImage} className='w-full shadow' alt="" />
                         </div>
                         
                     </div>
                     <div className='input-field-addstudent'>
                         <label htmlFor="username">Username:</label>
-                        <input type="text" className={inputStyle} id='username' placeholder='Student username...' />
+                        <input type="text" onChange={e => !idAsUsername && setUname(e.target.value)} value={uname} className={inputStyle} id='username' placeholder='Student username...' disabled={idAsUsername ? true : ''} />
                     </div>
-                    <div className='input-field-addstudent'>
+                    <div className='input-field-addstudent py-1 pb-2'>
                         <div>
                             
                         </div>
                         <div className='flex gap-2'>
-                            <input type="checkbox" id="useStudentId"/>
-                            <label htmlFor="useStudentId" className='py-2'>Use Student ID as username</label>
+                            <input type="checkbox" onChange={e => setIdAsUsername(e.target.checked)} id="useStudentId"/>
+                            <label htmlFor="useStudentId" className=' text-xs'>Use Student ID as username</label>
                         </div>
                             
                     </div>
                     <div className='input-field-addstudent'>
                         <label htmlFor="password">Password:</label>
-                        <input type="password" className={inputStyle} id='password' placeholder='Password...' />
+                        <input type="password" onChange={e => setPassword(e.target.value)} value={password} className={inputStyle} id='password' placeholder='Password...' />
                     </div>
                 </div>
             </div>
@@ -149,7 +168,10 @@ const AddStudent = () => {
                 <button type="submit" className='border-2 border-green-500 text-white bg-green-500 hover:text-green-500 hover:bg-transparent py-1 px-3 rounded font-semibold duration-200 text-sm shadow-sm'>Add Student</button>
                 <span className='font-medium text-xs text-gray-400'>Your admin id is: { data._id }</span>
             </div>
-
+            <p>{ studentId }</p>
+            <p>{ uname }</p>
+            
+            
         </form>
     </div>
   )
